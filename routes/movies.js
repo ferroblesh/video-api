1aa078adc3dd49513d4b79d2b396e3b7caac5df7
Fernando Robles <ferroblesh@gmail.com>
2020-12-29 21:59:45 -0600

    Added crud operations

--- routes/movies.js
+++ routes/movies.js
@@ -16,6 +16,54 @@ function moviesApi(app) {
             next(error);
         }
     });
+
+    router.get('/:movieID', async function(req,res,next) {
+        try {
+            const movies = await Promise.resolve(moviesMock[0]);
+            res.status(200).json({
+                data: movies,
+                message: 'movie'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.post('/', async function(req,res,next) {
+        try {
+            const createMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(201).json({
+                data: createMovieId,
+                message: 'movies list'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.put('/:movieId', async function(req,res,next) {
+        try {
+            const updatedMovieId = await Promise.resolve(moviesMock[0]);
+            res.status(200).json({
+                data: updatedMovieId,
+                message: 'movie updated'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.delete('/:movieId', async function(req,res,next) {
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock);
+            res.status(200).json({
+                data: deletedMovieId,
+                message: 'movie deleted'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
 }
 
 module.exports = moviesApi;
